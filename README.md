f4fd59e22988dbab3feeb9401a9784b14a511da8
Aravindhan <arvnd13@gmail.com>
2024-07-28 01:08:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# media_rendering_test1" 
---
+++ render_image_audio_and_video/test_pi.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Child care Videos")
+st.subheader("Image1:")
+st.image("image1.jpg")
+st.subheader("audio1:")
+st.audio("audio.mp3");
+st.subheader("video1: ");
+st.video("video.mp4");
