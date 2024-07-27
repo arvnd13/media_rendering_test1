cd87a1f66bc17712d8fc850efd983a819c4328b7
arvnd13 <arvnd13@gmail.com>
2024-07-28 02:19:13 +0530

    Update test_pi.py

--- render_image_audio_and_video/test_pi.py
+++ render_image_audio_and_video/test_pi.py
@@ -1,8 +1,8 @@
 import streamlit as st
-st.title("Child care Videos")
-st.subheader("Image1:")
-st.image("image1.jpg")
-st.subheader("audio1:")
-st.audio("audio.mp3");
-st.subheader("video1: ");
-st.video("video.mp4");
+#st.title("Child care Videos")
+#st.subheader("Image1:")
+#st.image("https://github.com/arvnd13/media_rendering_test1/blob/main/render_image_audio_and_video/image1.jpg")
+#st.subheader("audio1:")
+#st.audio("audio.mp3");
+st.subheader("Test video playback - : ");
+st.video("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4", format="video/mp4")
